d01ab94c081c4fa40bde51321d849a8660ae26af
Vivian Zhu <vivian.yijia.zhu@gmail.com>
2023-08-02 20:57:39 -0700

    Tets2

--- isracecarbounded.py
+++ isracecarbounded.py
@@ -1,57 +1,3 @@
-"""
-Copyright MIT BWSI Autonomous RACECAR Course
-MIT License
-Summer 2023
-
-Code Clash #16 - Is racecar bounded? (isracecarbounded.py)
-
-
-Author: Koneshka Bandyopadhyay
-
-Difficulty Level: 8/10
-
-
-Prompt: You are competing for a GrandPrix with other racecars and
-to make things interesting, there are multiple ways to get to the 
-finish line. However, some pathways may lead to traps, or loops.
-Your job is to identify if your racecar is stuck in a circle or loop.
-On an infinite plane, a racecar initially stands at [0, 0] and faces north.
-Note that:
-
-The north direction is the positive direction of the y-axis.
-The south direction is the negative direction of the y-axis.
-The east direction is the positive direction of the x-axis.
-The west direction is the negative direction of the x-axis.
-The robot can receive one of three instructions:
-
-"G": go straight 1 unit.
-"L": turn 90 degrees to the left (i.e., anti-clockwise direction).
-"R": turn 90 degrees to the right (i.e., clockwise direction).
-The robot performs the instructions given in order, and repeats them forever.
-
-Return true if and only if there exists a circle in the plane such 
-that the robot never leaves the circle.
-
-HINT: in simple words, it forms a loop that includes the point of [0,0] (starting point).
-However, remember that the loop may be formed with multiple executions of the instructions
-and not just one. Keep in mind that a circle can take multiple runs
-of the same instruction to form.
-
-Test Cases:
-Input: "GG" Output: False
-This will yield false as it simply goes in one direction and isn't bounded by any "circle" in the plane.
-
-
-Input: "GLL" Output: True
-This will yield true even though it doesn't form a circle in the first execution,
-after multiple executations, it will eventually reach the starting position of [0,0] 
-and form a loop after the 2nd execution.
-
-Input: "GLGGRRGG" Output: True
-This will yield true even though it doesn't form a circle in the first execution,
-after multiple executations, it will eventually reach the starting position of [0,0] 
-and form a loop after the 4th execution. Figure out why it's after the 4th execution.
-    """
 class Solution:
     def isracecarbounded(self, instructions):
             #type instructions: string
@@ -62,25 +8,51 @@ class Solution:
             cur_dir=2
             
             #TODO: Write code below to returnn a boolean value with the solution to the prompt.
-            for i in range(4):
-                for i in range(len(instructions)):
-                    if(instructions[i] =='G'):
-                        if(cur_pos==0):
-                            cur_pos[1]-=1
-                        elif(cur_dir==1):
-                            cur_pos[0]-=1
-                        elif(cur_dir==2):
-                            cur_pos[1]+=1
-                        else:
-                            cur_pos[0]+=1
-                    elif(instructions[i]== 'R'):
-                        cur_dir+=1
-                    else:
-                        cur_dir-=1 
-                    if(cur_pos==(0,0)):
-                        return True
-            return False
-                    
+            for x in range(4):
+                 for i in instructions: 
+                        if i == "G":
+                            if cur_dir == 2:
+                                cur_pos[1] = cur_pos[1] + 1
+                            if cur_dir == 0: 
+                                cur_pos[1] = cur_pos[1]-1
+                            if cur_dir == 3:
+                                 cur_pos[0] = cur_pos[0]+1
+                            if cur_dir ==1:
+                                 cur_pos[0] = cur_pos[0]-1
+                        if i == "L":
+                             if cur_dir == 0: 
+                                  cur_dir =3
+                             else: 
+                                  cur_dir = cur_dir -1
+                        if i == "R":
+                             if cur_dir ==3: 
+                                  cur_dir = 0
+                             else: 
+                                  cur_dir = cur_dir + 1
+            if cur_pos == initial_pos:
+                 return True 
+            else: 
+                 return False 
+            # previousChar = previousChar = instructions[0]
+            # answer = False
+            # print("instructions: " + instructions)
+            # for i in range(len(instructions)):
+            #      if i == 0: 
+            #           continue
+                      
+            #      else:
+                      
+            #           if ((previousChar == instructions[i] and instructions[i] == "L") or (previousChar == instructions[i] and instructions[i] == "R")):
+                           
+            #                answer = True
+            #                previousChar = instructions[i]
+            #                print("prev char: " + previousChar)
+            #                break 
+            
+            # return answer
+                      
+            pass
+        
 def main():
     input1=input()
     tc1 = Solution()
